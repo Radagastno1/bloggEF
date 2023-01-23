6b764709bdb944bb5e72da095934cce9bc85a311
Radagastno1 <angelinaholmqvist@live.se>
2023-01-23 16:32:08 +0100

    authorservice

--- LOGIC/AuthorService.cs
+++ LOGIC/AuthorService.cs
@@ -3,13 +3,34 @@ namespace LOGIC;
 public class AuthorService
 {
     IRepository<Author> _authorRepository;
+    IRepository<Blog> _blogRepository;
 
-    public AuthorService(IRepository<Author> authorRepository)
+    public AuthorService(IRepository<Author> authorRepository, IRepository<Blog> blogRepository)
     {
         _authorRepository = authorRepository;
+        _blogRepository = blogRepository;
     }
     public IEnumerable<Author> GetAllAuthors()
     {
         return _authorRepository.GetAll();
+    }
+    public Author GetAuthorById(int id)
+    {
+        return _authorRepository.GetById(id);
+    }
+    public void UpdateAuthor(Author author)
+    {
+        _authorRepository.Update(author);
+    }
+    public void DeleteAuthor(int id)
+    {
+        _authorRepository.Delete(id);
+    }
+    public void AssignBlogToAuthor(int authorId, int blogId)
+    {
+        Author author = _authorRepository.GetById(authorId);
+        Blog blog = _blogRepository.GetById(blogId);
+        author.Blogs.Add(blog);
+        _authorRepository.Update(author);
     }
 }
