1770ccf38e69b8ac8ef42d2cc3b347441bf0296d
Radagastno1 <angelinaholmqvist@live.se>
2023-01-23 18:59:05 +0100

    added loginrepository

---
+++ DATABASE/Repositories/LogInRepository.cs
@@ -0,0 +1,16 @@
+using LOGIC;
+using CORE;
+
+namespace DATABASE.Repositories;
+public class LogInRepository : ILogInRepository
+{
+    MyDbContext _db;
+    public LogInRepository(MyDbContext db)
+    {
+        _db = db;
+    }
+    public Author TryLogIn(string email, string password)
+    {
+        return _db.Authors.Where(a => a.Email == email && a.Password == password).FirstOrDefault();
+    }
+}
